ad5f0e3d3377b82140c513247a49aee1df0ce002
Yadi <ashleyfreedenberg@gmail.com>
2019-03-05 20:25:29 -0800

    Work from 3/5

--- assets/javascript/javaScript.js
+++ assets/javascript/javaScript.js
@@ -5,25 +5,53 @@ function displayBreedGif() {
     var breed = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + breed + "&api_key=zNBZZJ2RtFXneRDIDVhJ8CSDtHISleX4&limit=10";
     
+    $("#gifSpot").empty();
+    
     $.ajax({
         url: queryURL,
-        method: "GET" 
-    }).then(function(response){
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
-        
+
         for (var i = 0; i < response.data.length; i++) {
-            //var rating = response.data[i].rating; 
 
-            var gifUrl = response.data[i].images.fixed_height_still.url; 
+            var rating = response.data[i].rating;
+            var gifUrl = response.data[i].images.fixed_height_still.url;
+            var gifUrlAnimate = response.data[i].images.fixed_height.url;
+            console.log(rating);
+            console.log(gifUrl);
             
-            //var rateText = $("<p>");
-           // rateText.html("Rating: " + rating);
 
-            var gifStill = $("<img>");
-            gifStill.attr("src", gifUrl);
-            gifStill.attr("alt", "dog gif");
-           
-            $("#gifSpot").append(gifStill);
+            var figure = $("<figure class='figure'>");
+
+            var img = $("<img class='gifImg'>");
+            img.attr("src", gifUrl);
+            img.attr("alt", "dog gif");
+            img.attr("data-state", "still")
+            img.attr("data-still", gifUrl);
+            img.attr("data-amimate", gifUrlAnimate);
+
+            var caption = $("<figcaption class='figure-caption'>");
+    
+            caption.text("Rating: " + rating);
+
+            figure.append(caption);
+            figure.append(img);
+            $("#gifSpot").prepend(figure);
+
+            $(".gifImg").on("click", function() {
+                
+                var state = $(this).attr("data-state");
+                console.log(state);
+                
+                //if (state === "still") {
+                  //$(this).attr("src", $(this).attr("data-animate"));
+                  //$(this).attr("data-state", "animate");
+                //} else {
+                  //$(this).attr("src", $(this).attr("data-still"));
+                  //$(this).attr("data-state", "still");
+                //}
+              });
         }
     });
 }
@@ -31,9 +59,9 @@ function displayBreedGif() {
 
 function renderButtons() {
     $("#btnSpot").empty();
-    
 
-    for (var i = 0; i < topics.length; i++){
+
+    for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
         a.addClass("btn");
         a.attr("data-name", topics[i]);
@@ -42,7 +70,7 @@ function renderButtons() {
     }
 }
 
-$("#add-breed").on("click", function(event) {
+$("#add-breed").on("click", function (event) {
     event.preventDefault();
     var breed = $("#breed-input").val().trim();
     topics.push(breed);
